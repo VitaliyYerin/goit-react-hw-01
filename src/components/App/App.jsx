dfe557ab80351f42ff1efbf78bd2622bd21f8e59
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-07-25 12:30:34 +0300

    First Commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import user from 'data/user';
+import stats from 'data/data';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+
+import Profile from '../Profile/Profile';
+import Statistics from '../Statistics/Statistics';
+import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+
+import s from './App.module.css';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.mainContainer}>
+      <div className={s.profileContainer}>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={stats} />
+      </div>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
